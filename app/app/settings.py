16f3a2c0498005378d8173300860524723247745
sumedhbhaskar <sumedh.bhaskar@gmail.com>
2022-08-04 14:41:49 +0530

    added custom users and usermanager

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -136,3 +137,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
